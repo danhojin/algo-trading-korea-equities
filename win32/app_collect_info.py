2bf2778347a69a1cdb8cdcff4fa87d41ad33979c
Hojin Dan <danhojin@gmail.com>
2019-04-08 07:29:49 +0900

    rename db table and column names, files. add environment variables

--- win32/app_collect_info.py
+++ win32/app_collect_info.py
@@ -2,9 +2,15 @@ from PyQt5 import QtWidgets
 from PyQt5 import QAxContainer
 from collect_info_ui import Ui_CollectInfo
 
+from collections import deque
 import itertools
+from datetime import datetime
 from functools import partial
-import models
+
+import json
+from kafka import KafkaProducer
+import db
+from settings import KW_CONTROL_CLSID, KAFKA_BOOTSTRAP_SERVER
 
 
 KW_MARKET = {
@@ -25,11 +31,11 @@ class AppCollectInfo(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
         self.kiwoom = QAxContainer.QAxWidget(
-            '{A1574A0D-6BFA-4BD7-9020-DED88711818D}'
+            KW_CONTROL_CLSID
         )
-        self.kiwoom.OnEventConnect.connect(
-            self.kw_on_event_connect
-        )
+        self.kiwoom.OnEventConnect.connect(self.on_event_connect)
+        self.kiwoom.OnReceiveRealData.connect(self.on_receive_real_data)
+
         self.ui = Ui_CollectInfo()
         self.ui.setupUi(self)
         for symbol in KW_MARKET:
@@ -38,41 +44,95 @@ class AppCollectInfo(QtWidgets.QWidget):
                     self.button_get_code_list_clicked,
                     KW_MARKET,
                     symbol)
-            )        
+            )
+        self.ui.obs_btn_set_real_reg.clicked.connect(
+            self.obs_btn_set_real_reg_cb
+        )
+        self.ui.obs_btn_set_real_remove.clicked.connect(
+            self.obs_btn_set_real_remove_cb
+        )
 
+        self.obs_cells = deque([], maxlen=10)
+        self.kafka_producer = KafkaProducer(
+            bootstrap_servers=[KAFKA_BOOTSTRAP_SERVER],
+            value_serializer=lambda v: json.dumps(v).encode('utf-8')
+        )
         self.kiwoom.dynamicCall('CommConnect()')
 
-    def kw_on_event_connect(self, err_code):
+    def on_receive_real_data(self, s_code, s_real_type, s_real_data):
+        if s_real_type == '주식체결':
+            price = self.kiwoom.dynamicCall(
+                'GetCommRealData(QString, int)',
+                s_code,
+                10
+            )
+            cum_volume = self.kiwoom.dynamicCall(
+                'GetCommRealData(QString, int)',
+                s_code,
+                13
+            )
+            now = datetime.now().strftime('%H:%M:%S')
+            self.obs_cells.append((
+                now,
+                price,
+                cum_volume
+            ))
+            
+            self.kafka_producer.send(
+                'StockTradingPrice',
+                { 'event': now, 'price': price, 'cum_volume': cum_volume }
+            )
+
+            self.obs_table_refresh()
+
+    def obs_table_refresh(self):
+        for i, data in enumerate(self.obs_cells):
+            for j, v in enumerate(data):
+                self.ui.obs_table.setItem(i, j, QtWidgets.QTableWidgetItem(v))
+
+    def obs_btn_set_real_reg_cb(self):
+        code = self.ui.obs_line_code.text()
+        self.kiwoom.dynamicCall(
+            "SetRealReg(QString, QString, QString, QString)",
+            "0150", code, "9001;10;13", "0")
+
+    def obs_btn_set_real_remove_cb(self):
+        self.kiwoom.dynamicCall(
+            'SetRealRemove(QString, QString)',
+            'All', 'All'
+        )
+
+    def on_event_connect(self, err_code):
         if err_code == 0:
             print('Connected')
         else:
             print(err_code)
 
     def button_get_code_list_clicked(self, market, symbol):
-        stocks = self.kiwoom.dynamicCall(
+        assets = self.kiwoom.dynamicCall(
             'GetCodeListByMarket(QString)',
             market[symbol]
         )
-        stocks = map(lambda x: x.strip(), stocks.split(';'))
-        stocks = filter(len, stocks)
-        stocks = [{
+        assets = map(lambda x: x.strip(), assets.split(';'))
+        assets = filter(len, assets)
+        assets = [{
             'code': code,
             'name': self.kiwoom.dynamicCall(
                 'GetMasterCodeName(QString)', code
-            ).strip()} for code in stocks
+            ).strip()} for code in assets
         ]
 
-        models.insert_into_Stock(stocks)
+        db.insert_into_Asset(assets)
 
         market_indices = [{
             'symbol': symbol,
             'stock': code,
         } for (symbol, code) in zip(
             itertools.repeat(symbol),
-            [s['code'] for s in stocks]
+            [s['code'] for s in assets]
         )]
 
-        models.insert_into_MarketIndex(market_indices)
+        db.insert_into_MarketIndex(market_indices)
         print('db session closed')
 
 if __name__ == '__main__':
--- win32/db.py
+++ win32/db.py
@@ -6,7 +6,7 @@ db = orm.Database(
     create_db=True
 )
 
-class Stock(db.Entity):
+class Asset(db.Entity):
     code = orm.PrimaryKey(str, auto=True)
     name = orm.Optional(str)
     market_indices = orm.Set('MarketIndex')
@@ -15,7 +15,7 @@ class Stock(db.Entity):
 class MarketIndex(db.Entity):
     id = orm.PrimaryKey(int, auto=True)
     symbol = orm.Required(str)
-    stock = orm.Required(Stock)
+    asset = orm.Required(Asset)
 
 # turn on debug mode
 # orm.sql_debug(True)
@@ -23,15 +23,15 @@ class MarketIndex(db.Entity):
 db.generate_mapping(create_tables=True)
 
 @orm.db_session
-def insert_into_Stock(stocks):
-    for stock in stocks:
-        s = Stock.get(code=stock['code'])
-        if s is None:
-            Stock(**stock)
+def insert_into_Asset(assets):
+    for asset in assets:
+        a = Asset.get(code=asset['code'])
+        if a is None:
+            Asset(**asset)
 
 @orm.db_session
 def insert_into_MarketIndex(market_indices):
     for index in market_indices:
-        i = MarketIndex.get(symbol=index['symbol'], stock=index['stock'])
+        i = MarketIndex.get(symbol=index['symbol'], asset=index['asset'])
         if i is None:
             MarketIndex(**index)
