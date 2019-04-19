12c190a7c1fd734b53cdeb142410bec294690ed4
Hojin Dan <danhojin@gmail.com>
2019-04-19 13:39:05 +0900

    remove heading space

--- market_scraping/daily_asset_prices.sh
+++ market_scraping/daily_asset_prices.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/bash
- cat < list_tiger200.csv | awk -F',' '{if(NR>1) print$1}' | xargs -I {} scrapy crawl daily_asset_prices -L INFO -a code={}
+cat < list_tiger200.csv | awk -F',' '{if(NR>1) print$1}' | xargs -I {} scrapy crawl daily_asset_prices -L INFO -a code={}
