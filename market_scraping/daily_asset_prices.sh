d4c2ffafaf426be541fd5a069f7396f4c5259762
Hojin Dan <danhojin@gmail.com>
2019-05-14 12:45:13 +0900

    minor

--- market_scraping/daily_asset_prices.sh
+++ market_scraping/daily_asset_prices.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/bash
-cat < kosdaq150.csv | awk -F',' '{if(NR>1) print$1}' | xargs -I {} scrapy crawl daily_asset_prices -L INFO -a code={}
+cat < list_kospi200.csv | awk -F',' '{if(NR>1) print$1}' | xargs -I {} scrapy crawl daily_asset_prices -L INFO -a code={}
