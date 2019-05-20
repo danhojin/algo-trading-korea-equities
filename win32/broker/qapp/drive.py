3320bfc1811ad91926d376a928b8d56fa6845696
Hojin Dan <danhojin@gmail.com>
2019-05-20 15:43:17 +0900

    add deco in broker

--- win32/broker/qapp/drive.py
+++ win32/broker/qapp/drive.py
@@ -3,8 +3,21 @@ from PyQt5 import QAxContainer
 from PyQt5 import QtCore
 from broker.qapp.register_ui import Ui_Form
 
+import datetime
+
 # KHOPENAPI.KHOpenAPICtrl.1
-KW_CONTROL_CLSID = 'A1574A0D-6BFA-4BD7-9020-DED88711818D'
+# KW_CONTROL_CLSID = 'A1574A0D-6BFA-4BD7-9020-DED88711818D'
+KW_CONTROL_CLSID = 'KHOPENAPI.KHOpenAPICtrl.1'
+
+
+def on_receive_real(real_type):
+    def _deco(func):
+        def wrapper(*args, **kwargs):
+            if real_type == args[2]:
+                func(*args, **kwargs)
+            return
+        return wrapper
+    return _deco
 
 
 class RegisterForm(QtWidgets.QWidget):
@@ -18,7 +31,8 @@ class RegisterForm(QtWidgets.QWidget):
         )
         self.kiwoom.OnEventConnect.connect(self.on_event_connect)
         self.kiwoom.OnReceiveTrData.connect(self.on_receive_tr_data_cb)
-        self.kiwoom.OnReceiveRealData.connect(self.on_receive_real_data_cb)
+        self.kiwoom.OnReceiveRealData.connect(self.on_real_traded)
+        # self.kiwoom.OnReceiveRealData.connect(self.on_real_bidask)
 
         self.ui = Ui_Form()
         self.ui.setupUi(self)
@@ -39,26 +53,39 @@ class RegisterForm(QtWidgets.QWidget):
         else:
             print(err)
 
-    def on_receive_tr_data_cb(self, screen_no, rqname, trcode, recordname, prev_next, data_len, err_code, msg1, msg2):
-        if rqname == 'BalanceRq':
-            total = self.kiwoom.dynamicCall('CommGetData(QString, QString, QString, int, QString', trcode, '', rqname, 0, '총평가금액')
+    def on_receive_tr_data_cb(self, s_screen_number, s_rq_name,
+                              s_tr_code, s_record_name, s_prev_next, *_):
+        if s_rq_name == 'BalanceRq':
+            total = self.kiwoom.dynamicCall(
+                'CommGetData(QString, QString, QString, int, QString',
+                s_tr_code, '', s_rq_name, 0, '총평가금액')
             print(total)
 
-    def on_receive_real_data_cb(self, s_code, s_real_type, s_real_data):
-        if s_real_type == '주식체결':
-            s_real_data = s_real_data.strip().split()
-            print('traded: ', s_real_data)
-        elif s_real_type == '주식호가잔량':
-            s_real_data = s_real_data.strip().split()
-            print('bid/ask: ', s_real_data)
-            
+    @on_receive_real('주식체결')
+    def on_real_traded(self, s_code, s_real_type, s_real_data):
+        s_real_data = s_real_data.strip().split()
+        deal = dict()
+        deal['time'] = (s_real_data[0][:2], s_real_data[0][2:4], s_real_data[0][4:])
+        deal['time'] = datetime.time(*map(lambda x: int(x), deal['time']))
+        deal['quote'] = float(s_real_data[1])
+        deal['cum_volume'] = float(s_real_data[7])
+
+    @on_receive_real('주식호가잔량')
+    def on_real_bidask(self, s_code, s_real_type, s_real_data):
+        s_real_data = s_real_data.strip().split()
+        print('bida', '-'*10, s_real_data[:3])
 
     def balance_info(self):
-        self.kiwoom.dynamicCall('SetInputValue(QString, QString)', '계좌번호', self.accounts[0])
-        self.kiwoom.dynamicCall('SetInputValue(QString, QString)', '비밀번호', '')
-        self.kiwoom.dynamicCall('SetInputValue(QString, QString)', '비밀번호입력매체구분', '00')
-        self.kiwoom.dynamicCall('SetInputValue(QString, QString)', '조회구분', '1')
-        self.kiwoom.dynamicCall('CommRqData(QString, QString, QString, QString)', 'BalanceRq', 'opw00018', '0', '8000')
+        self.kiwoom.dynamicCall('SetInputValue(QString, QString)',
+                                '계좌번호', self.accounts[0])
+        self.kiwoom.dynamicCall('SetInputValue(QString, QString)',
+                                '비밀번호', '')
+        self.kiwoom.dynamicCall('SetInputValue(QString, QString)',
+                                '비밀번호입력매체구분', '00')
+        self.kiwoom.dynamicCall('SetInputValue(QString, QString)',
+                                '조회구분', '1')
+        self.kiwoom.dynamicCall('CommRqData(QString, QString, QString, QString)',
+                                'BalanceRq', 'opw00018', '0', '8000')
         print('RqCommitted')
 
     def listening_q(self):
@@ -67,9 +94,14 @@ class RegisterForm(QtWidgets.QWidget):
 
     def button_register_cb(self):
         code = self.ui.codeLineEdit.text()
+        # s_screen_no
+        # s_code_list
+        # s_fid_list
+        # s_opt_type 0 for initial reg, 1 for add list
         self.kiwoom.dynamicCall(
             'SetRealReg(QString, QString, QString, QString)',
-            '0150', code, '9001;302;10;13;27;28;41;61', '0'
+            # '0150', code, '9001;302;10;13;27;28;41;61', '0'
+            '0150', code, '10;13;61', '0'
         )
 
 
